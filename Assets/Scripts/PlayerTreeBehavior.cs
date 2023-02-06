3b742f96dbd483a7555173d7a4c5c4a59a3aa1ab
Bakakemono <robinalves1224@gmail.com>
2023-02-06 14:57:31 +0100

    Micro update on tree script and now trying to have fun with vector for the next steps

--- Assets/Scripts/PlayerTreeBehavior.cs
+++ Assets/Scripts/PlayerTreeBehavior.cs
@@ -5,9 +5,37 @@ using UnityEngine.U2D;
 
 public class PlayerTreeBehavior : MonoBehaviour
 {
-    List<SpriteShapeController> roots = new List<SpriteShapeController>();
+    [SerializeField] SpriteShapeController[] roots;
+
+    [Header("Tree Parameters")]
+    int nmbRootsAvailable = 4;
+
+    [Header("Root Parameters")]
+    [SerializeField] float maxLengthPerRoot = 40.0f;
+    [SerializeField] float propagationSpeed = 10.0f;
+    [SerializeField] float segmentMaxLength = 3.0f;
+
+    Vector2 currentDirection = Vector2.zero;
+
+    Vector2 input = Vector2.zero;
+    int framerate;
 
     private void Start() {
+        framerate = (int)(1.0f / Time.fixedDeltaTime);
+    }
+
+    private void Update() {
+        CaptureInpute();
+    }
+
+    private void FixedUpdate() {
         
+    }
+    
+    void CaptureInpute() {
+        input = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")
+            );
     }
 }
