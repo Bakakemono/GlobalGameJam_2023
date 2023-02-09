4dcf4c2fbc59c3c5818c1efadc5986490f17da1b
Robin Alves <robinalves1224@gmail.com>
2023-02-09 20:07:30 +0100

    Add multiple root for the player to manager but strange direction behavior bug remain

--- Assets/Scripts/PlayerTreeBehavior.cs
+++ Assets/Scripts/PlayerTreeBehavior.cs
@@ -12,16 +12,22 @@ public class PlayerTreeBehavior : MonoBehaviour
     }
 
     [SerializeField] SpriteShapeController[] roots;
+    int[] rootsSegmentNmbs;
 
     [Header("Tree Parameters")]
-    int nmbRootsAvailable = 4;
+    int maxRootNmb = 5;
+    int availableRootNmb = 4;
+    int currentRootIndex = 0;
 
     [Header("Root Parameters")]
     [SerializeField] float maxLengthPerRoot = 40.0f;
     [SerializeField] float propagationSpeed = 10.0f;
     [SerializeField] float segmentMaxLength = 3.0f;
+    [SerializeField] float segmentMinLength = 1.0f;
+    [SerializeField] float segmentCurrentLength = 0;
+    [SerializeField] float rootCurrentLength = 0;
 
-    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDir = Vector2.right;
     Vector2 newDir;
     float maxAngle = 45.0f;
 
@@ -31,6 +37,12 @@ public class PlayerTreeBehavior : MonoBehaviour
 
     private void Start() {
         framerate = (int)(1.0f / Time.fixedDeltaTime);
+
+        rootsSegmentNmbs = new int[maxRootNmb];
+        for(int i = 0; i < roots.Length; i++) {
+            rootsSegmentNmbs[i] = roots[i].spline.GetPointCount();
+        }
+        AddNewPointToSpline(currentRootIndex);
     }
 
     private void Update() {
@@ -38,7 +50,27 @@ public class PlayerTreeBehavior : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        DirCheckerAndCorrecter();
+        DirCheckerAndCorrector();
+
+        roots[currentRootIndex].spline.SetPosition(
+            rootsSegmentNmbs[currentRootIndex] - 1,
+            roots[currentRootIndex].spline.GetPosition(rootsSegmentNmbs[currentRootIndex] - 1) + (Vector3)currentDir * (propagationSpeed * Time.fixedDeltaTime)
+            );
+
+        for(int i = 0; i < rootsSegmentNmbs[currentRootIndex]; i++) {
+            roots[currentRootIndex].spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+        }
+
+        rootCurrentLength += (propagationSpeed * Time.fixedDeltaTime);
+
+        segmentCurrentLength += (propagationSpeed * Time.fixedDeltaTime);
+
+        if(newDir != Vector2.zero && newDir != currentDir && segmentCurrentLength > segmentMinLength) {
+            AddNewPointToSpline(currentRootIndex);
+        }
+        else if(segmentCurrentLength >= maxLengthPerRoot) {
+            AddNewPointToSpline(currentRootIndex);
+        }
     }
     
     void CaptureInpute() {
@@ -49,24 +81,23 @@ public class PlayerTreeBehavior : MonoBehaviour
         newDir = input;
     }
 
-    void DirCheckerAndCorrecter() {
+    void DirCheckerAndCorrector() {
         if(newDir == Vector2.zero)
             return;
 
         if(Vector2.Angle(currentDir, newDir) > maxAngle) {
             Side side = CheckSide(currentDir, newDir);
             if(side == Side.RIGHT) {
-                currentDir = new Vector2(-newDir.y, newDir.x);
+                currentDir = (currentDir + new Vector2(-newDir.y, newDir.x)).normalized;
                 return;
             }
             else if(side == Side.RIGHT) {
-                currentDir = new Vector2(newDir.y, -newDir.x);
+                currentDir = (currentDir + new Vector2(newDir.y, -newDir.x)).normalized;
                 return;
             }
         }
         if(newDir != currentDir)
-
-        currentDir = newDir;
+            currentDir = newDir;
     }
 
     // Check if the new direction is to the right or to the left of the current direction
@@ -78,17 +109,17 @@ public class PlayerTreeBehavior : MonoBehaviour
         return directionCheck > 0 ? Side.RIGHT : directionCheck < 0 ? Side.LEFT : Side.NEITHER;
     }
 
-    //void AddNewPointToSpline() {
-    //    currentSegmentLength = 0;
-    //    spriteShapeController.spline.InsertPointAt(
-    //        rootSplinePointNmb - 1,
-    //        spriteShapeController.spline.GetPosition(rootSplinePointNmb - 2) +
-    //        (spriteShapeController.spline.GetPosition(rootSplinePointNmb - 1) - spriteShapeController.spline.GetPosition(rootSplinePointNmb - 2)) * 0.95f
-    //        );
-    //    rootSplinePointNmb++;
-    //    spriteShapeController.spline.SetTangentMode(rootSplinePointNmb - 2, ShapeTangentMode.Continuous);
-    //    if(rootSplinePointNmb > 10) {
-    //        spriteShapeController.spline.SetTangentMode(rootSplinePointNmb - 10, ShapeTangentMode.Linear);
-    //    }
-    //}
+    void AddNewPointToSpline(int rootIndex) {
+        segmentCurrentLength = 0;
+        roots[rootIndex].spline.InsertPointAt(
+            rootsSegmentNmbs[rootIndex] - 1,
+            roots[rootIndex].spline.GetPosition(rootsSegmentNmbs[rootIndex] - 2) +
+            (roots[rootIndex].spline.GetPosition(rootsSegmentNmbs[rootIndex] - 1) - roots[rootIndex].spline.GetPosition(rootsSegmentNmbs[rootIndex] - 2)) * 0.95f
+            );
+        rootsSegmentNmbs[rootIndex]++;
+        roots[rootIndex].spline.SetTangentMode(rootsSegmentNmbs[rootIndex] - 2, ShapeTangentMode.Continuous);
+        //if(rootsSegmentNmbs[rootIndex] > 10) {
+        //    roots[rootIndex].spline.SetTangentMode(rootsSegmentNmbs[rootIndex] - 10, ShapeTangentMode.Linear);
+        //}
+    }
 }
